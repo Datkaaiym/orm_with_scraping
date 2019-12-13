442a8c8d5699c102486dcd565e539f7c2e8e2116
Datkaaiym <datka777888@mail.ru>
2019-12-13 20:58:31 +0600

    create////

---
+++ models.py
@@ -0,0 +1,35 @@
+from peewee import *
+from datetime import datetime
+
+user = 'newuser'
+password = 'password'
+db_name = 'kenesh'
+
+db = MySQLDatabase(
+    user = user, password = password,
+    database = db_name
+)
+db.connect()
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Deputy(BaseModel):
+    id = PrimaryKeyField(primary_key = True)
+    name = CharField(max_length = 100)
+    number = CharField(max_length = 100)
+    created_at = DateTimeField(
+        default = datetime.now()
+    )
+    bio = TextField()
+    
+    class Meta:
+        db_table = 'deputy_data'
+        order_by = ('created_at', )
+
+try:
+    Deputy.create_table()
+
+except InternalError as px:
+    print(str(px))
